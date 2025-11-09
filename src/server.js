f17797493722fa9efdfb70be4680d478cc91de4c
nasti <anastasiasaienko634@gmail.com>
2025-11-09 10:43:41 +0100

    done

--- src/server.js
+++ src/server.js
@@ -1,14 +1,20 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
-
+import productRouter from './routes/products.js';
 import { connectMongoDB } from './db/connectMongoDB.js';
-
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
 const app = express();
 const PORT = process.env.PORT ?? 3030;
 
 app.use(express.json());
 app.use(cors());
+app.use(productRouter);
+
+app.use(notFoundHandler);
+
+app.use(errorHandler);
 
 await connectMongoDB();
 app.listen(PORT, () => {
